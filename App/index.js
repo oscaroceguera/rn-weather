c69dc32b7cdd2c3ec4b31f8a437e1297d6be2f33
oscar <oscaroceguera@gmail.com>
2020-10-11 22:22:08 -0600

    init project

---
+++ App/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default () => (
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text>View the finished branch for finished code.</Text>
+  </View>
+);
